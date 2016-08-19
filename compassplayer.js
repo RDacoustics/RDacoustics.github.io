83d643c22883d721129e9019131da6084a75a947
RomainDumoulin <romain.dumoulin@ens.etsmtl.ca>
2016-08-19 02:57:01 -0400

    test GPS

--- compassplayer.js
+++ compassplayer.js
@@ -220,3 +220,18 @@ $(document).ready(function() {
 		rotator2.updateRotMtx();	
 	    rotator3.updateRotMtx();		
 	};
+	
+	
+	
+	function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
