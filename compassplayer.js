96f76d74b3984724baea507366a1b1ca211413ed
RomainDumoulin <romain.dumoulin@ens.etsmtl.ca>
2016-08-22 00:43:40 -0400

    o

--- compassplayer.js
+++ compassplayer.js
@@ -268,8 +268,8 @@ function IsRecinSourceCircle(pointxy,circle) {
 //circle equation, with rec x.y: greater than, less than, or equal to the r^2 value. 
 //If it is greater, then the point lies outside of the circle
 
-oo = Math.pow((pointxy[0] - circle[0]),2) + Math.pow((pointxy[1] - circle[1]),2)-Math.pow(circle[3],2) ;
-oo=  Math.pow((pointxy[0] - circle[0]),2) + Math.pow((pointxy[1] - circle[1]),2);
+oo = Math.pow((pointxy[0] - circle[0]),2) + Math.pow((pointxy[1] - circle[1]),2)- Math.pow(circle[3],2) ;
+oo=  Math.pow((pointxy[0] - circle[0]),2) + Math.pow((pointxy[1] - circle[1]),2) - Math.pow(circle[3],2);
 return oo 
 
   //  if ((pointxy[0] - (circle[0]*circle[0])) + (pointxy[1] - (circle[1]*circle[1])) <= (circle[3]*circle[3]))  {
